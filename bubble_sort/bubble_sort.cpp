69c6b58143c852fb172f0d5f7235031c78539aa9
emzhran <zachran750@gmail.com>
2023-03-16 23:42:13 +0700

    tambahan void bubblesortarray

--- bubble_sort/bubble_sort.cpp
+++ bubble_sort/bubble_sort.cpp
@@ -27,6 +27,18 @@ void input() {
 	}
 }
 
+void bubblesortarray() {
+	for (int i = 1; i < n; i++) {
+		for (int j = 0; j < n - i; j++) {
+			if (a[j] > a[j + 1]) {
+				int temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
+			}
+		}
+	}
+}
+
 int main()
 {
 }
