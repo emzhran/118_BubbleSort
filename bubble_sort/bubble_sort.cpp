4ba9c35f5e0ce085451f7318086ab4f5189503ae
emzhran <zachran750@gmail.com>
2023-03-16 23:46:21 +0700

    tambahan void display

--- bubble_sort/bubble_sort.cpp
+++ bubble_sort/bubble_sort.cpp
@@ -50,6 +50,16 @@ void unsorted() {
 	cout << endl;
 }
 
+void display() {
+	cout << endl;
+	cout << "================================" << endl;
+	cout << "Elemen array yang belum tersusun" << endl;
+	cout << "================================" << endl;
+	for (int j = 0; j < n; j++) {
+		cout << a[j] << endl;
+	}
+	cout << endl;
+}
 int main()
 {
 }
